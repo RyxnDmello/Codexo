2d0b20f0bfb85eee5e4359101401875c12c891a4
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-02-09 23:35:09 +0530

    CODEXO V1.1.4.1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const editors = require("./json/editors.json");
-const features = require("./json/features.json");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -23,17 +22,24 @@ app.get("/learn/editors/heavy", (req, res) => {
     title: editors.heavy.title,
     image: editors.heavy.image,
     description: editors.heavy.description,
-    description: editors.heavy.description,
     collectionTitle: editors.heavy.collectionTitle,
     collection: editors.heavy.collection,
-
-    featuresTitle: features.heavy.featuresTitle,
-    features: features.heavy,
+    featuresTitle: editors.heavy.featuresTitle,
+    featuresRows: editors.heavy.featuresRows,
   });
 });
 
 app.get("/learn/editors/light", (req, res) => {
-  res.render("editors", { editors: editors.heavy });
+  res.render("editors", {
+    type: editors.light.type,
+    title: editors.light.title,
+    image: editors.light.image,
+    description: editors.light.description,
+    collectionTitle: editors.light.collectionTitle,
+    collection: editors.light.collection,
+    featuresTitle: editors.light.featuresTitle,
+    featuresRows: editors.light.featuresRows,
+  });
 });
 
 app.listen(PORT, () => {
