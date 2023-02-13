6c901bb8f1e93a6c6e6df17d77541b36b944fc6e
RYAN NOLASCO D MELLO <ryanndmello10@gmail.com>
2023-02-13 22:12:37 +0530

    CODEXO V1.1.8.4

--- JavaScript/register/SwitchForms.js
+++ JavaScript/register/SwitchForms.js
@@ -1,4 +1,4 @@
-const forms = document.querySelectorAll(".form.create, .form.login");
+let switchText = document.querySelector(".info-link-button-text.switch");
 const switchButton = document.querySelector(".info-link-button.switch");
 const formsContainer = document.querySelector(".form-container");
 const formsCarousel = document.querySelector(".forms-carousel");
@@ -7,22 +7,21 @@ let welcome = 0;
 let slides = 0;
 
 export default function SwitchForms() {
-  WelcomeAnimation();
+  Welcome();
+  Switch();
+}
 
+function Switch() {
   switchButton.addEventListener("click", () => {
     if (slides !== 0) slides = 0;
     else slides = 1;
 
-    formsContainer.style.width = "0px";
-
-    setTimeout(() => {
-      formsCarousel.style.translate = -slides * 600 + "px 0";
-      formsContainer.style.width = "600px";
-    }, 650);
+    ChangeForms();
+    ChangeButtonText();
   });
 }
 
-function WelcomeAnimation() {
+function Welcome() {
   if (welcome === 1) return;
 
   formsContainer.style.width = "0px";
@@ -31,4 +30,26 @@ function WelcomeAnimation() {
     formsContainer.style.width = "600px";
     welcome = 1;
   }, 600);
+}
+
+function ChangeForms() {
+  formsContainer.style.width = "0px";
+
+  setTimeout(() => {
+    formsCarousel.style.translate = -slides * 600 + "px 0";
+    formsContainer.style.width = "600px";
+  }, 650);
+}
+
+function ChangeButtonText() {
+  switchText.style.opacity = "0";
+
+  setTimeout(() => {
+    if (slides === 0) switchText.textContent = "Login";
+    else switchText.textContent = "Signup";
+  }, 500);
+
+  setTimeout(() => {
+    switchText.style.opacity = "1";
+  }, 1000);
 }
